5ca12068449ce9cf37115af574df095628cb6008
asgerkappel <asge2431@mail.hansenberg.dk>
2023-12-18 14:46:54 +0100

    start movement

--- Assets/Flat Platformer Template/Scripts/Enemies.cs
+++ Assets/Flat Platformer Template/Scripts/Enemies.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Enemy : MonoBehaviour
+public class Enemies : MonoBehaviour
 {
-     public float WalkSpeed;
+    public float WalkSpeed;
     public float JumpForce;
     public AnimationClip _walk, _jump;
     public Animation _Legs;
@@ -15,24 +15,41 @@ public class Enemy : MonoBehaviour
 
     public int maxHP = 100;
     private int currentHP;
-    public int damage = 10; // Skade forårsaget af fjenden
-   
+
+    // Referencer
+    public Blade blade;
+
+    // Metode til at tage skade
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+
+        // Her kan du tilføje yderligere logik, fx spilleren dør, hvis HP når nul.
+        if (currentHP <= 0)
+        {
+            // slet fjenden, hvis den dør
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Slet fjenden, når den dør
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Blade")) // Antag, at klingen har en tag kaldet "Blade"
+        {
+            // Hent skadeværdien fra klingen og påfør fjenden skaden
+            TakeDamage(collision.gameObject.GetComponent<Blade>().damage);
+        }
+    }
 
     private void Update()
     {
         MoveEnemy(); // Kald funktionen for fjendens bevægelse
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Player player = other.GetComponent<Player>();
-            if (player != null)
-            {
-                player.TakeDamage(damage); // Skade påføres spilleren ved kollision med fjenden
-            }
-        }
     }
 
     private void MoveEnemy()
@@ -41,4 +58,3 @@ public class Enemy : MonoBehaviour
         // Dette kan omfatte brug af Rigidbody2D og MovePosition for at bevæge fjenden mod spilleren eller i et forudbestemt mønster.
     }
 }
-
--- Assets/Flat Platformer Template/Scripts/Player.cs
+++ Assets/Flat Platformer Template/Scripts/Player.cs
@@ -26,6 +26,7 @@ public class Player : MonoBehaviour
         if (currentHP <= 0)
         {
             // Implementer game over eller død-logik her, fx deaktivér spillerens kontrol.
+            // Eksempel: gameObject.SetActive(false);
             gameObject.SetActive(false);
         }
     }
@@ -69,53 +70,56 @@ public class Player : MonoBehaviour
     }
 
     void FixedUpdate()
+{
+    // Input fra tastaturet
+    float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+    // Retningen, som spilleren bevæger sig mod
+    Vector2 moveDirection = new Vector2(horizontalInput, 0);
+
+    // Normaliser retningen for at undgå bevægelse med forskellige hastigheder diagonalt
+    moveDirection.Normalize();
+
+    // Bevægelse baseret på tastaturinput (A og D)
+    rig.velocity = new Vector2(moveDirection.x * WalkSpeed * Time.deltaTime, rig.velocity.y);
+
+    // Spejlvendt scaling og rotation baseret på bevægelsesretning
+    if (horizontalInput > 0)
     {
-        Vector3 dir = cam.ScreenToWorldPoint(Input.mousePosition) - _Blade.transform.position;
-        dir.Normalize();
-
-        if (cam.ScreenToWorldPoint(Input.mousePosition).x > transform.position.x + 0.2f)
-            mirror = false;
-        if (cam.ScreenToWorldPoint(Input.mousePosition).x < transform.position.x - 0.2f)
-            mirror = true;
-
-        if (!mirror)
-        {
-            rot = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.localScale = new Vector3(_startScale, _startScale, 1);
-            _Blade.transform.rotation = Quaternion.AngleAxis(rot, Vector3.forward);
-        }
-        if (mirror)
-        {
-            rot = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg;
-            transform.localScale = new Vector3(-_startScale, _startScale, 1);
-            _Blade.transform.rotation = Quaternion.AngleAxis(rot, Vector3.forward);
-        }
-
-        if (_inputAxis.x != 0)
-        {
-            rig.velocity = new Vector2(_inputAxis.x * WalkSpeed * Time.deltaTime, rig.velocity.y);
-
-            if (_canWalk)
-            {
-                _Legs.clip = _walk;
-                _Legs.Play();
-            }
-        }
-
-        else
-        {
-            rig.velocity = new Vector2(0, rig.velocity.y);
-        }
-
-        if (_isJump)
-        {
-            rig.AddForce(new Vector2(0, JumpForce));
-            _Legs.clip = _jump;
-            _Legs.Play();
-            _canJump = false;
-            _isJump = false;
-        }
+        mirror = false;
+        transform.localScale = new Vector3(_startScale, _startScale, 1);
     }
+    else if (horizontalInput < 0)
+    {
+        mirror = true;
+        transform.localScale = new Vector3(-_startScale, _startScale, 1);
+    }
+
+    // Afspilning af gang-animation, hvis spilleren bevæger sig
+    if (horizontalInput != 0 && _canWalk)
+    {
+        _Legs.clip = _walk;
+        _Legs.Play();
+    }
+
+    // Hoplogik
+    if (_inputAxis.y > 0 && _canJump)
+    {
+        _canWalk = false;
+        _isJump = true;
+    }
+
+    // Jump handling (kan tilpasses efter behov)
+    if (_isJump)
+    {
+        rig.AddForce(new Vector2(0, JumpForce));
+        _Legs.clip = _jump;
+        _Legs.Play();
+        _canJump = false;
+        _isJump = false;
+    }
+}
+
 
     public bool IsMirror()
     {
