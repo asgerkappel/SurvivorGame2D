dd47a2801ec01d2db1da3bd8a1af8131e46e8df0
Sebastian Thomsen <seba9323@mail.hansenberg.dk>
2023-12-20 12:36:58 +0100

    Audio semi færdig

--- Assets/Flat Platformer Template/Scripts/Menues/SettingsMenu.cs
+++ Assets/Flat Platformer Template/Scripts/Menues/SettingsMenu.cs
@@ -2,13 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-    public void SetVolume(float volume)
-    {
-        // Set volume in audio mixer
-        audioMixer.SetFloat("volume", volume);
+    public void Start(){
+        if(PlayerPrefs.HasKey("Volume")){
+            LoadVolume();
+        }
+        else{
+        SetVolume();
+        }
     }
+
+    public void SetVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("Volume", volume);
+    }
+
+    private void LoadVolume(){
+        musicSlider.value=PlayerPrefs.GetFloat("Volume");
+        SetVolume();
+    }
+
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
 }
