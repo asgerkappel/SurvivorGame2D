2faef93a32a0b276bdb3b5469745578efc555cdc
asgerkappel <asge2431@mail.hansenberg.dk>
2023-12-20 09:00:21 +0100

    blade danser

--- Assets/Flat Platformer Template/Scripts/Blade.cs
+++ Assets/Flat Platformer Template/Scripts/Blade.cs
@@ -5,8 +5,31 @@ using UnityEngine;
 public class Blade : MonoBehaviour
 {
     public int damage = 100; 
+    // rotation på x-aksen
+    public float xRotation = 0f;
+    
+    void Update()
+    {
+        // Roter klingen omkring x-aksen
+        transform.Rotate(new Vector3(1, 0, 0));
+    }
 
-    
-   
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        // Hvis klingen rammer en fjende, så skal fjenden tage skade
+        if (other.CompareTag("Enemy"))
+        {
+            // Hent fjendens script
+            Enemies enemy = other.GetComponent<Enemies>();
+            
+            // Hvis fjenden har et script, så påfør skade
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+    }
+
+
+
 }
