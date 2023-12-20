b434da8c90723e9b48d87d549d5f4ee73edcd8f0
asgerkappel <asge2431@mail.hansenberg.dk>
2023-12-20 11:53:12 +0100

    slags animation halvvejs

--- Assets/Flat Platformer Template/Scripts/Blade.cs
+++ Assets/Flat Platformer Template/Scripts/Blade.cs
@@ -5,31 +5,56 @@ using UnityEngine;
 public class Blade : MonoBehaviour
 {
     public int damage = 100; 
-    // rotation på x-aksen
-    public float xRotation = 0f;
     
+    private bool isRotating = false;
+    private float startRotation = 34.714f;
+    private float endRotation = 0f;
+    private float rotationSpeed = 500f;
+    private float waitTime = 0.5f;
+
     void Update()
     {
-        // Roter klingen omkring x-aksen
-        transform.Rotate(new Vector3(1, 0, 0));
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // Hvis klingen rammer en fjende, så skal fjenden tage skade
-        if (other.CompareTag("Enemy"))
+        if (Input.GetKeyDown(KeyCode.Space) && !isRotating)
         {
-            // Hent fjendens script
-            Enemies enemy = other.GetComponent<Enemies>();
-            
-            // Hvis fjenden har et script, så påfør skade
-            if (enemy != null)
-            {
-                enemy.TakeDamage(damage);
-            }
+            StartCoroutine(RotateObject());
         }
     }
 
+    IEnumerator RotateObject()
+    {
+        isRotating = true;
 
+        while (transform.rotation.eulerAngles.z > endRotation)
+        {
+            float newRotation = Mathf.MoveTowardsAngle(transform.rotation.eulerAngles.z, endRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, newRotation);
+            yield return null;
+        }
 
+        yield return new WaitForSeconds(waitTime);
+
+        while (transform.rotation.eulerAngles.z < startRotation)
+        {
+            float newRotation = Mathf.MoveTowardsAngle(transform.rotation.eulerAngles.z, startRotation, rotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, newRotation);
+            yield return null;
+        }
+
+        isRotating = false;
+    }
 }
+
+
+
+
+
+
+
+        // hvis den er mirror så skal startRotation være -34.714f;
+
+
+
+      /*  // Sværdet danser
+        transform.Rotate(new Vector3(1, 0, 0)); */
+
+
