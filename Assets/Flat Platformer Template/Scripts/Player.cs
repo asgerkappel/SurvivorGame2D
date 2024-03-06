c1892728fe62d441b0aad48d57704d691a8890fe
Sebastian Thomsen <seba9323@mail.hansenberg.dk>
2024-03-06 13:42:37 +0100

    Lidt oprydning enemy kan dø

--- Assets/Flat Platformer Template/Scripts/Player.cs
+++ Assets/Flat Platformer Template/Scripts/Player.cs
@@ -32,7 +32,7 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.tag == "Enemy"){
-            TakeDamage(damage);
+            enemyHP.TakeDamage(damage);
         }
     }
 
