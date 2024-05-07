feacae6b66c43c55a76881705bf68844c9745ae7
asgerkappel <asge2431@mail.hansenberg.dk>
2024-05-07 12:48:46 +0200

    er klar til at lave neste leve

--- Assets/Flat Platformer Template/Scripts/Windmill_end_level.cs
+++ Assets/Flat Platformer Template/Scripts/Windmill_end_level.cs
@@ -1,19 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Windmill_end_level : MonoBehaviour
 {
-    // Reference to the level manager
-    public LevelManager levelManager;
-
+    
     // Example of detecting player interaction using OnTriggerEnter
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("Cart"))
         {
-            // Call the CompleteLevel function in the level manager
-            levelManager.CompleteLevel();
+            Debug.Log("Level completed!");
+       
+            UnityEngine.SceneManagement.SceneManager.LoadScene("endscreen");
         }
     }
 }
