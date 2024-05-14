274d7ee1e30f91db87e4897dd66c57c6e93af02f
Sebastian Thomsen <seba9323@mail.hansenberg.dk>
2024-05-14 08:27:13 +0200

    dwa

--- Assets/Flat Platformer Template/Scripts/Player.cs
+++ Assets/Flat Platformer Template/Scripts/Player.cs
@@ -38,11 +38,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.tag == "Enemy"){
-            enemyHP.TakeDamage(damage);
-        }
-    }
 
     private bool _canJump, _canWalk;
     private bool _isWalk, _isJump;
